7e271756ba37d1b95bda54a3c90147daa9914984
pazera66 <k.p.krol@stud.elka.pw.edu.pl>
2016-07-07 00:06:02 +0200

    first commit

---
+++ src/main/java/com/example/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@Configuration
+@EnableAutoConfiguration
+
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String showText(){
+		return "Test Spring Boot JAVA!aaaaaaaaaaaaaaaa";
+	}
+}
